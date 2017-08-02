44bd47e2e104b8d56be8261f873ed8487bb5c31f
Timothy Reen <reentim@gmail.com>
2017-08-02 15:06:20 +1000

    Add a test for named groups.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'named group membership' do
+    user = User.create!
+
+    user.named_groups.add(:admin)
+
+    assert_includes(user.named_groups, :admin)
+  end
 end
